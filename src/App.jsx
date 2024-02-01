9dba204953b77113f6f96018ac20c73749cef15c
Miguel Cobian <miguelcobian99@gmail.com>
2024-02-01 13:27:34 -0600

    Add. Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
+import { Header } from "./components/Header"
 
 function App() {
 
   return (
-    <h1 className="text-6xl font-extrabold">Hola mundo</h1>
+    <div className="my-20 max-w-lg mx-auto bg-white shadow p-10">
+      <Header />
+      <input
+        type="range"
+        className="w-full h-6 bg-gray-600 accent-lime-500 hover:accent-lime-600"
+      />
+    </div>
   )
 }
 
