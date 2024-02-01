0995b2a3ab8a7536227c11c94354434ecda42b8a
Miguel Cobian <miguelcobian99@gmail.com>
2024-02-01 14:40:31 -0600

    Add. Select with the time to pay

--- src/App.jsx
+++ src/App.jsx
@@ -55,6 +55,18 @@ function App() {
         className="w-full h-6 bg-gray-600 accent-lime-500 hover:accent-lime-600"
       />
       <p className='text-center my-10 text-5xl font-extrabold text-indigo-600'>{formatearDinero(cantidad)}</p>
+
+      <h2 className='text-2xl font-extrabold text-gray-500 text-center'>
+        Elige un <span className='text-indigo-600'>plazo</span> a pagar
+      </h2>
+
+      <select
+        className='mt-5 w-full p-2 bg-white border border-gray-300 rounded-lg text-center text-xl font-bold text-gray-500'
+      >
+        <option value="6">6 meses</option>
+        <option value="12">12 meses</option>
+        <option value="24">24 meses</option>
+      </select>
     </div>
   )
 }
